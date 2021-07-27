fb064e478d1a52037d42569eddf04ea12f1a8129
Lockey-J <zhijia21cn@163.com>
2021-07-27 17:05:54 +0800

    Update 还原成中文字符.js

--- AST/还原成中文字符.js
+++ AST/还原成中文字符.js
@@ -10,9 +10,9 @@ const traverse = require("@babel/traverse").default;
 const t = require("@babel/types");
 const generator = require("@babel/generator").default;
 
-let jscode = "var s = \"\u4f60\u597d\uff0c\u4e16\u754c;\"\n" +
-    "var a = \"\u0068\u0065\u006c\u006c\u006f\u002c\u0020\u0077\u006f\u0072\u0064\";";
-
+let jscode = fs.readFileSync("./ddj.js", {
+    encoding: "utf-8"
+});
 let ast = parse(jscode);
 
 const visitor =
@@ -28,4 +28,6 @@ const visitor =
 
 traverse(ast,visitor);
 let {code} = generator(ast, {jsescOption:{"minimal":true}});
-console.log(code);
+fs.writeFileSync('./after.js', code,{
+    encoding :'utf-8'
+})
